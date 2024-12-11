59250716df418a28bedf675a3842092fdc4ed583
Matthew Brooker <matthewbrooker@Matthews-MacBook-Pro-2.local>
2024-12-11 06:54:54 -0500

    request animation frame>

--- src/systems/renderer.js
+++ src/systems/renderer.js
@@ -2,14 +2,12 @@ import { gameState } from './gameState.js';
 
 export function drawBackground() {
     const ctx = gameState.canvas.context;
-    // Check if the image has loaded
     if (gameState.images.background.complete) {
-        // Draw the image to fill the canvas
         ctx.drawImage(gameState.images.background, 0, 0, gameState.canvas.width, gameState.canvas.height);
     } else {
-        // If the image hasn't loaded yet, use a solid color as fallback
         ctx.fillStyle = "green";
         ctx.fillRect(0, 0, gameState.canvas.width, gameState.canvas.height);
+        gameState.images.background.onload = () => requestAnimationFrame(gameLoop);
     }
 }
 
